3547b3f15998b2d55b39bf5956bd6b28ef2e6924
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-09-10 14:34:01 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -4,9 +4,9 @@ include "view-header.php";
 ?>
     <h1>Post Result</h1>
 <?php
-if (isset($_POST['my-name']))
-{?> 
-<p> The Value Sent Is! </p>
+if (isset($_POST['my-name'])) {
+    ?> 
+<p> The Value Sent Is!: </p>
 <?php
  echo $_POST['my-name'];}
 else{

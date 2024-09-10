bf5d1649dd787a9162adc82fd760c0eb717df848
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-09-10 12:12:52 -0500

    Create post.php

---
+++ post.php
@@ -0,0 +1,12 @@
+<?php
+$pageTitle = "Post";
+include "view-header.php";
+?>
+    <h1>Post</h1>
+<form Method="post" Action="post-result.php">
+  <input type="text" Name="my-name">
+  <input type="submit" value="Post data">
+  </form>
+<?php
+include "view-footer.php";
+?>

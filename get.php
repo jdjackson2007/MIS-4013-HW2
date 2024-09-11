07ad31635d1427bb316c75e0bb65e019893a1b8f
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-09-10 20:50:46 -0500

    Create get.php

---
+++ get.php
@@ -0,0 +1,12 @@
+<?php
+$pageTitle = "Get";
+include "view-header.php";
+?>
+    <h1>Get</h1>
+<form Method="get" Action="get-result.php">
+  <input type="text" Name="my-name">
+  <input type="submit" value="Get data">
+  </form>
+<?php
+include "view-footer.php";
+?>

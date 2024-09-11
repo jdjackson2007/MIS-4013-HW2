858b522d5f63edec9d62575b685b8922245f0c8d
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-09-10 21:33:50 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -5,14 +5,17 @@ include "view-header.php";
     <h1>Post Result</h1>
 <?php
 echo getDisplay();
-
 include "view-footer.php";
 
 function getDisplay() {
-    if (isset($_POST['my-name'])) { 
+   
+    if (isset($_POST['my-name'])) 
+    { 
 return "<p> The Value Sent Is!: </p>" . $_POST['my-name'];
-}else{
+}
+    else{
  return "<p> Nothing Posted To The Page! </p>";
-<?php}
+<?php
+    }
 }
 ?>
